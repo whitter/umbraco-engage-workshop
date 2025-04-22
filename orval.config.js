b6bfc947d59b346e9bdf3bf816c98e8101fd17fc
Phil Whittaker <phil@hifi.agency>
2025-04-22 08:17:02 +0100

    format orval

--- orval.config.js
+++ orval.config.js
@@ -1,57 +1,57 @@
 module.exports = {
-    'umbraco-transfomer': {
-      output: {
-        mode: 'tags-split',
-        target: './src/api/client.ts',
-        baseUrl: 'http://localhost:23142/',
-        schemas: './src/api/model',
-        client: 'fetch',
-        override: {
-            mutator: {
-                path: './src/custom-fetch.ts',
-                name: 'customFetch',
-            },
-        },
-      },
-      input: {
-        target: 'http://localhost:23142/umbraco/swagger/delivery/swagger.json',
+  'umbraco-transfomer': {
+    output: {
+      mode: 'tags-split',
+      target: './src/api/client.ts',
+      baseUrl: 'http://localhost:23142/',
+      schemas: './src/api/model',
+      client: 'fetch',
+      override: {
+          mutator: {
+              path: './src/custom-fetch.ts',
+              name: 'customFetch',
+          },
       },
     },
-    //this won't run whilst the umbraco ommunity poackage is present
-    /*'engage-transfomer': { 
-      output: {
-        mode: 'tags-split',
-        target: './src/api-engage/client.ts',
-        baseUrl: 'http://localhost:23142/',
-        schemas: './src/api-engage/model',
-        client: 'fetch',
-        override: {
-            mutator: {
-                path: './src/custom-fetch.ts',
-                name: 'customFetch',
-            },
-        },
+    input: {
+      target: 'http://localhost:23142/umbraco/swagger/delivery/swagger.json',
+    },
+  },
+  //this won't run whilst the umbraco ommunity poackage is present
+  /*'engage-transfomer': { 
+    output: {
+      mode: 'tags-split',
+      target: './src/api-engage/client.ts',
+      baseUrl: 'http://localhost:23142/',
+      schemas: './src/api-engage/model',
+      client: 'fetch',
+      override: {
+          mutator: {
+              path: './src/custom-fetch.ts',
+              name: 'customFetch',
+          },
       },
-      input: {
-        target: 'http://localhost:23142/umbraco/swagger/engage-api/swagger.json?urls.primaryName=Umbraco+Engage+API',
+    },
+    input: {
+      target: 'http://localhost:23142/umbraco/swagger/engage-api/swagger.json?urls.primaryName=Umbraco+Engage+API',
+    },
+  },*/
+  'clean-starter-transfomer': {
+    output: {
+      mode: 'tags-split',
+      target: './src/api-clean/client.ts',
+      baseUrl: 'http://localhost:23142/',
+      schemas: './src/api-clean/model',
+      client: 'fetch',
+      override: {
+          mutator: {
+              path: './src/custom-fetch.ts',
+              name: 'customFetch',
+          },
       },
-    },*/
-    'clean-starter-transfomer': {
-      output: {
-        mode: 'tags-split',
-        target: './src/api-clean/client.ts',
-        baseUrl: 'http://localhost:23142/',
-        schemas: './src/api-clean/model',
-        client: 'fetch',
-        override: {
-            mutator: {
-                path: './src/custom-fetch.ts',
-                name: 'customFetch',
-            },
-        },
-      },
-      input: {
-        target: 'http://localhost:23142/umbraco/swagger/clean-starter/swagger.json?urls.primaryName=Clean+starter+kit',
-      },
-    }
-  };
+    },
+    input: {
+      target: 'http://localhost:23142/umbraco/swagger/clean-starter/swagger.json?urls.primaryName=Clean+starter+kit',
+    },
+  }
+};
